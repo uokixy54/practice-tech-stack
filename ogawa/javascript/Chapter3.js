cfc5b89196f28ce6f36f47e5eb16f537568c5e4c
Y-ogawa <yuto5656@outlook.jp>
2026-01-26 23:12:48 +0900

    📝Chapter3 学習１

---
+++ ogawa/javascript/Chapter3.js
@@ -0,0 +1,12 @@
+// オブジェクト
+let shimabu = {
+    name: "しまぶー", // プロパティ
+    teachprogramming: function() {}, // メソッド
+};
+console.log(shimabu);
+
+// 後から追加・変更が可能
+shimabu.name = "しまぶーのIT大学"; // 追加
+shimabu.age = 20; // 変更
+
+console.log(shimabu);

e0f72f72ebe7189c63e1b487567c908e3d1c1169
uokixy54 <uokixy.lemberry@gmail.com>
2026-02-26 02:54:30 +0900

    ✏️簡単なアプリケーション作成

---
+++ majima/rust/minigrep/src/lib.rs
@@ -0,0 +1,29 @@
+use std::io::prelude::*;
+use std::{error::Error, fs::File};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text: \n{}", contents);
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Self { query, filename })
+    }
+}

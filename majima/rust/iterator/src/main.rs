28ed85ff50d7ab4af2bec9c494b4ec4d86eb01a9
uokixy54 <uokixy.lemberry@gmail.com>
2026-03-01 19:53:41 +0900

    ✏️イテレータについて学習

---
+++ majima/rust/iterator/src/main.rs
@@ -0,0 +1,9 @@
+// イテレータ
+fn main() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    // コレクションメソッドでvecに戻す
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
